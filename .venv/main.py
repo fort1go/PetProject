6b37dde620550e5f8b63e68ef1d52d6224f5e03d
fort1go <egor.kononov.2007@mail.ru>
2024-08-17 14:17:26 +1000

    Checkpoint 17.08 14:17 W

--- .venv/main.py
+++ .venv/main.py
@@ -3,14 +3,16 @@ from pydantic import BaseModel
 from datetime import datetime
 from random import randint
 
+id_length = 6
+
 
 class Model(BaseModel):
-    login: str
-    password: str
+    login: str = 'aa'
+    password: str = 'aa'
     second_name: str = 'second_name'
     first_name: str = 'first_name'
     surname: str = 'surname'
-    DoB: datetime = datetime("2024-01-01")
+    DoB: datetime = datetime(2024,1,1)
     Expirience: float = 0
 
 
@@ -19,29 +21,31 @@ database = {"id": "BaseModel" # ['login', 'password', 'second_name', 'first_name
 
 
 
-app = FastAPI()
+app = FastAPI() # uvicorn main:app --reload
 
-@app.get("/profile/{profile_id}")
-def read_profile(profile_id: int, password: str, login: str):
-    if profile_id in database:
+@app.get("/")
+def hihihaha(test: str):
+    return f"Main page: {test}"
+
+@app.get("/profile/{id}")
+def read_profile(id: int, password: str, login: str):
+    if id in database:
         if password == database[id].password and login == database[id].login:
             birthdate = database[id].DoB
             age = datetime.now().year - birthdate.year - ((datetime.now().month, datetime.now().day) < (birthdate.month, birthdate.day))
-            return {database[id].model_dump(), f'Возраст = {age}'}
+            return (database[id].model_dump(), f'Возраст = {age}')
         else:
             return "Неправильный логин или пароль"
     else:
         return "Такого профиля не существует"
 
 @app.post("/profile")
-def registration(profile: Model, password: str, login: str):
+def registration(password: str, login: str):
+    profile = Model(password=password, login=login)
     while True:
-        id = int(''.join([str(randint(0, 9)) for _ in range(6)])) # id = случайное шестизначное число
+        id = int(''.join([str(randint(0, 9)) for _ in range(id_length)])) # id = случайное {id_length}-значное число
         if id not in database: # и проверяется наличие идентичного id в database
             break
 
-    database[id] = profile()
-    return {f"Аккаунт создан\n"
-            f"id = {id}\n"
-            f"{profile.model_dump()}"
-            }
+    database[id] = profile
+    return {f"Аккаунт создан;  id = {id}; {profile.model_dump()}"}
