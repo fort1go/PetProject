9a8d728d0448d531463082811663aa2e6b771bc5
fort1go <egor.kononov.2007@mail.ru>
2024-08-17 16:46:08 +1000

    Checkpoint 17.08 16:44 W POST + GET + PATCH - PUT - DELETE -

--- .venv/main.py
+++ .venv/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Body
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 from datetime import datetime
 from random import randint
 
@@ -8,13 +8,13 @@ id_length = 6
 
 
 class Model(BaseModel):
-    login: str = 'aa'
-    password: str = 'aa'
-    second_name: str = 'second_name'
-    first_name: str = 'first_name'
-    surname: str = 'surname'
-    DoB: datetime = datetime(2024,1,1)
-    Expirience: float = 0
+    login: str
+    password: str
+    second_name: str = None # 'second_name'
+    first_name: str = None # 'first_name'
+    surname: str = None # 'surname'
+    DoB: datetime = None # datetime(2020,1,1)
+    Expirience: float = None # 0
 
 
 database = {"id": "BaseModel" # ['login', 'password', 'second_name', 'first_name', 'surname', 'DoB', 'Experience']
@@ -29,20 +29,18 @@ def hihihaha(data=Body()):
     return f"Main page: {data}"
 
 @app.get("/profile/{id}")
-def read_profile(id: int, data=Body()):
+def read_profile(id: int):
     if id in database:
-        if data["password"] == database[id].password and data["login"] == database[id].login:
-            birthdate = database[id].DoB
-            age = datetime.now().year - birthdate.year - ((datetime.now().month, datetime.now().day) < (birthdate.month, birthdate.day))
+        if (birthdate := database[id].DoB) != None:
+            age = datetime.now().year - birthdate.year - (
+                    (datetime.now().month, datetime.now().day) < (birthdate.month, birthdate.day))
             return (database[id].model_dump(), f'Возраст = {age}')
-        else:
-            return "Неправильный логин или пароль"
     else:
         return "Такого профиля не существует"
 
 @app.post("/profile")
 def registration(data=Body()):
-    profile = Model(password=data["password"], login=data["login"])
+    profile = Model(**data)
     while True:
         id = int(''.join([str(randint(0, 9)) for _ in range(id_length)])) # id = случайное {id_length}-значное число
         if id not in database: # и проверяется наличие идентичного id в database
